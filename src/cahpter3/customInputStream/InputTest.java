7d1e0c7e70ea9db1d6689edb6bd1f57f2714b7ea
akm4545 <akm4545@naver.com>
2022-11-02 10:53:20 +0900

    chapter3

---
+++ src/cahpter3/customInputStream/InputTest.java
@@ -0,0 +1,26 @@
+package cahpter3.customInputStream;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputTest {
+	public static void main(String[] args) throws IOException{
+		int c;
+		
+		try {
+			//테코레이터 패턴을 사용하여 read의 기능 확장을 함
+			InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("test.txt")));
+			
+			//한글자씩 읽으면서 출력
+			while((c = in.read()) >= 0) {
+				System.out.println((char)c);
+			}
+			
+			in.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
