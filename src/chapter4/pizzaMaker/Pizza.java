ab7aabfb20dd139b9aa0dbde011c5ad9e6acd434
akm4545 <akm4545@naver.com>
2022-11-02 16:23:43 +0900

    chapter4

--- src/chapter4/pizzaMaker/Pizza.java
+++ src/chapter4/pizzaMaker/Pizza.java
@@ -14,19 +14,26 @@ public abstract class Pizza {
 	}
 	
 	public void prepare() {
-		System.out.println("준비");
+		System.out.println("준비 중 : " + name);
+		System.out.println("도우를 돌리는 중...");
+		System.out.println("소스를 뿌리는 중...");
+		System.out.println("토핑을 올리는 중: ");
+		
+		for(String topping : toppings) {
+			System.out.println(" " + topping);
+		}
 	}
 	
 	public void bake() {
-		System.out.println("굽기");
+		System.out.println("175도에서 25분 간 굽기");
 	}
 	
 	public void cut() {
-		System.out.println("자르기");
+		System.out.println("피자를 사선으로 자르기");
 	}
 	
 	public void box() {
-		System.out.println("포장");
+		System.out.println("상자에 피자 담기");
 	}
 	
 	public String toString() {
--- src/chapter4/pizzaMaker/PizzaStore.java
+++ src/chapter4/pizzaMaker/PizzaStore.java
@@ -25,16 +25,24 @@ package chapter4.pizzaMaker;
 //}
 
 public abstract class PizzaStore {
-	 
+	
+	//추상 메소드로 선언하여 서브클래스가 객체 생성을 책임지도록 함
+	//특정 객체를 리턴하며 그 객체는 보통 슈퍼클래스가 정의한 메서드 내에서 쓰임
+	//팩토리 메소드는 실제로 생성되는 구상 객체가 무엇인지 알 수 없게 만드는 역할도 함
+	//매개변수로 만들 객체 종류를 선택할 수도 있음
 	public abstract Pizza createPizza(String item);
  
+	//피자 객체를 만드는 일은 PizzaStore의 서브클래스의 메소드에서 처리
 	public Pizza orderPizza(String type) {
 		Pizza pizza = createPizza(type);
+		
 		System.out.println("--- Making a " + pizza.getName() + " ---");
+		
 		pizza.prepare();
 		pizza.bake();
 		pizza.cut();
 		pizza.box();
+		
 		return pizza;
 	}
 }
