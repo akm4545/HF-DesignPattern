e33e8768798ac1d708577b284b51985fbb4154e2
akm4545 <akm4545@naver.com>
2022-11-02 12:17:57 +0900

    chapter4

--- src/chapter4/pizzaMaker/PizzaStore.java
+++ src/chapter4/pizzaMaker/PizzaStore.java
@@ -1,8 +1,19 @@
 package chapter4.pizzaMaker;
 
 public class PizzaStore {
-	Pizza orderPizza() {
-		Pizza pizza = new Pizza();
+	//팩토리 클래스에 객체 생성을 위임
+	SimplePizzaFactory factory;
+	
+	//생성자에 팩토리를 전달하여 저장
+	public PizzaStore(SimplePizzaFactory fecFactory) {
+		this.factory = factory;
+	}
+	
+	//타입을 받아 팩토리에서 객체를 생성
+	Pizza orderPizza(String type) {
+		Pizza pizza;
+		
+		pizza = factory.createPizza(type);
 		
 		pizza.prepare();
 		pizza.bake();
