2a41a5bdb5abdfaeb2d5e64d443582304f3eaa28
akm4545 <akm4545@naver.com>
2022-11-03 18:10:28 +0900

    chapter6

---
+++ src/chapter6/remote/SimpleRemoteControl.java
@@ -0,0 +1,17 @@
+package chapter6.remote;
+
+public class SimpleRemoteControl {
+	Command slot;
+	
+	public SimpleRemoteControl() {
+		
+	}
+	
+	public void setCommand(Command command) {
+		slot = command;
+	}
+	
+	public void buttonWasPressed() {
+		slot.execute();
+	}
+}
