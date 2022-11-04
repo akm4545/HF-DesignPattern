ad70cff36f72b0f91e35fad3e52dead0cd4eec28
akm4545 <akm4545@naver.com>
2022-11-04 18:37:18 +0900

    chpater6

--- src/chapter6/remote/RemoteLoader.java
+++ src/chapter6/remote/RemoteLoader.java
@@ -6,6 +6,20 @@ public class RemoteLoader {
 		
 		Light livingRoomLight = new Light("Living Room");
 		Light kitchenLight = new Light("Kitchen");
+		CeilingFan ceilingFan = new CeilingFan("Living Room");
+		GarageDoor garageDoor = new GarageDoor("Garage");
+		Stereo stereo = new Stereo("Living Room");
 		
+		LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+		LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+		LightOnCommand kitchenRoomLightOn = new LightOnCommand(kitchenLight);
+		LightOffCommand kitchenRoomLightOff = new LightOffCommand(kitchenLight);
+		
+		CeilingFanOnCommand ceilingFanOn = new CeilingFanOnCommand(ceilingFan);
+		CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
+		GarageDoorUpCommand garageDoorUp = new GarageDoorUpCommand(garageDoor);
+		GarageDoorDownCommand garageDoorDown = new GarageDoorDownCommand(garageDoor);
+		StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
+		StereoOffCommand stereoOff = new StereoOffCommand(stereo);
 	}
 }
