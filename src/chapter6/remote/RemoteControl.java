d2e5136a74c0452b811c52fb0130cd014d6feba8
akm4545 <akm4545@naver.com>
2022-11-06 15:28:53 +0900

    chapter6

--- src/chapter6/remote/RemoteControl.java
+++ src/chapter6/remote/RemoteControl.java
@@ -1,30 +1,38 @@
 package chapter6.remote;
 
 public class RemoteControl {
+	//커맨드를 저장하는 배열
 	Command[] onCommands;
 	Command[] offCommands;
 	
+	//생성자 
+	//커맨드 배열을 초기화 시킨다.
 	public RemoteControl() {
 		onCommands = new Command[7];
 		offCommands = new Command[7];
 		
 		Command noCommand = new NoCommand();
 		
+		//초기에는 배열에 아무것도 들어있지 않기 때문에 아무것도 안하는
+		//커맨드로 초기화 시킨다
 		for(int i=0; i<7; i++) {
 			onCommands[i] = noCommand;
 			offCommands[i] = noCommand;
 		}
 	}
 	
+	//원하는 커맨드 배열의 인덱스에 저장
 	public void setCommand(int slot, Command onCommand, Command offCommand) {
 		onCommands[slot] = onCommand;
 		offCommands[slot] = offCommand;
 	}
 	
+	//버튼을 누르면 커맨드의 execute를 실행
 	public void onButtonWasPushed(int slot) {
 		onCommands[slot].execute();
 	}
 	
+	//버튼을 누르면 커맨드의 execute를 실행
 	public void offButtonWasPushed(int slot) {
 		offCommands[slot].execute();
 	}
--- src/chapter6/remote/RemoteLoader.java
+++ src/chapter6/remote/RemoteLoader.java
@@ -4,12 +4,14 @@ public class RemoteLoader {
 	public static void main(String[] args) {
 		RemoteControl remoteControl = new RemoteControl();
 		
+		//각각의 기능을 구현한 객체를 생성
 		Light livingRoomLight = new Light("Living Room");
 		Light kitchenLight = new Light("Kitchen");
 		CeilingFan ceilingFan = new CeilingFan("Living Room");
 		GarageDoor garageDoor = new GarageDoor("Garage");
 		Stereo stereo = new Stereo("Living Room");
 		
+		//커맨드를 생성하면서 execute메소드에 어떤 일을 할지 저장함
 		LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
 		LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
 		LightOnCommand kitchenRoomLightOn = new LightOnCommand(kitchenLight);
@@ -21,5 +23,23 @@ public class RemoteLoader {
 		GarageDoorDownCommand garageDoorDown = new GarageDoorDownCommand(garageDoor);
 		StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
 		StereoOffCommand stereoOff = new StereoOffCommand(stereo);
+		
+		//리모컨에 커맨드를 저장
+		remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+		remoteControl.setCommand(1, kitchenRoomLightOn, kitchenRoomLightOff);
+		remoteControl.setCommand(2, ceilingFanOn, ceilingFanOff);
+		remoteControl.setCommand(3, stereoOnWithCD, stereoOff);
+		
+		System.out.println(remoteControl);
+		
+		//차례로 슬롯을 누름
+		remoteControl.onButtonWasPushed(0);
+		remoteControl.offButtonWasPushed(0);
+		remoteControl.onButtonWasPushed(1);
+		remoteControl.offButtonWasPushed(1);
+		remoteControl.onButtonWasPushed(2);
+		remoteControl.offButtonWasPushed(2);
+		remoteControl.onButtonWasPushed(3);
+		remoteControl.offButtonWasPushed(3);
 	}
 }
