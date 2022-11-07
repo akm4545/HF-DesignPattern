032d0bfb0f44cd9fc11d6df7c50e0c843e294865
akm4545 <akm4545@naver.com>
2022-11-07 17:31:34 +0900

    chapter8

--- src/chapter8/barista/Tea.java
+++ src/chapter8/barista/Tea.java
@@ -1,27 +1,38 @@
 package chapter8.barista;
 
-public class Tea {
+//public class Tea {
+public class Tea extends CaffeineBeverage {
 	
-	void prepareRecipe() {
-		boilWater();
-		steepTeaBag();
-		pourInCup();
-		addLemon();
-	}
+//	void prepareRecipe() {
+//		boilWater();
+//		steepTeaBag();
+//		pourInCup();
+//		addLemon();
+//	}
 	
-	public void boilWater() {
-		System.out.println("물 끓이는 중");
-	}
+//	public void boilWater() {
+//		System.out.println("물 끓이는 중");
+//	}
+//	
+//	public void steepTeaBag() {
+//		System.out.println("찻잎을 우려내는 중");
+//	}
 	
-	public void steepTeaBag() {
+//	public void addLemon() {
+//		System.out.println("레몬을 추가하는 중");
+//	}
+//	
+//	public void pourInCup() {
+//		System.out.println("컵에 따르는 중");
+//	}
+	
+	@Override
+	void brew() {
 		System.out.println("찻잎을 우려내는 중");
 	}
-	
-	public void addLemon() {
+
+	@Override
+	void addCondiments() {
 		System.out.println("레몬을 추가하는 중");
-	}
-	
-	public void pourInCup() {
-		System.out.println("컵에 따르는 중");
 	}
 }
