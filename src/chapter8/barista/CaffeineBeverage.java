c92d4742a5df8746c1c2728586bbf9d8b8612ff0
akm4545 <akm4545@naver.com>
2022-11-07 17:40:19 +0900

    chapter8

--- src/chapter8/barista/CaffeineBeverage.java
+++ src/chapter8/barista/CaffeineBeverage.java
@@ -1,6 +1,11 @@
 package chapter8.barista;
 
 //Coffee 클래스와 Tea 클래스가 공통으로 사용하는 메서드를 분리
+
+//템플릿 메소드 패턴(Template Method Pattern)
+//알고리즘의 골격을 정의
+//템플릿 메소드를 사용하면 알고리즘의 일부 단계를 서브클래스에서 구현할 수 있다.
+//알고리즘의 구조는 그대로 유지하면서 알고리즘의 특정 단계를 서브클래스에서 재정의할 수도 있다.
 public abstract class CaffeineBeverage {
 	
 	final void prepareRecipe() {
