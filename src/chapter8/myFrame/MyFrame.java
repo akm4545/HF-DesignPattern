1d661bffed7fc94d9e3b0fa88d5b623a394e6952
akm4545 <akm4545@naver.com>
2022-11-09 17:13:52 +0900

    chpater8

--- src/chapter8/myFrame/MyFrame.java
+++ src/chapter8/myFrame/MyFrame.java
@@ -4,6 +4,9 @@ import java.awt.Graphics;
 
 import javax.swing.JFrame;
 
+//update()메소드가 들어있는 JFrame을 확장 
+//update()메소드는 화면 갱신 알고리즘을 제어한다
+//paint()후크 메소드를 오버라이드 하면 그 알고리즘에 끼어들 수 있다.
 public class MyFrame extends JFrame{
 	
 	public MyFrame(String title) {
