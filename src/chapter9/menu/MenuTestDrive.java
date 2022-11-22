995c33df364e6bae643f8f68bc49d6bf2d99bccc
akm4545 <akm4545@naver.com>
2022-11-22 20:30:43 +0900

    chapter9

--- src/chapter9/menu/MenuTestDrive.java
+++ src/chapter9/menu/MenuTestDrive.java
@@ -1,5 +1,9 @@
 package chapter9.menu;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 //반복자 패턴(Iterator Pattern)
 //컬렉션의 구현 방법을 노출하지 않으면서 집합체 내의 모든
 //항목에 접근하는 방법을 제공
@@ -10,8 +14,14 @@ public class MenuTestDrive {
 		DinerMenu dinerMenu = new DinerMenu();
 		CafeMenu cafeMenu = new CafeMenu();
 		
+		List<Menu> menus = new ArrayList<Menu>();
+		
+		menus.add(pancakeHouseMenu);
+		menus.add(dinerMenu);
+		menus.add(cafeMenu);
+		
 		//종업원에게 넘긴 뒤
-		Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu, cafeMenu);
+		Waitress waitress = new Waitress(menus);
 		
 		//출력
 		waitress.printMenu();
--- src/chapter9/menu/Waitress.java
+++ src/chapter9/menu/Waitress.java
@@ -1,34 +1,55 @@
 package chapter9.menu;
 
+import java.util.Iterator;
+import java.util.List;
+
 //반복을 캡슐화 하였기 때문에 배열이나 List의 반환 타입을 신경 쓸 필요 없음
 public class Waitress {
 //	PancakeHouseMenu pancakeHouseMenu;
 //	DinerMenu dinerMenu;
 	
 	//종업원 클래스와 구상 클래스 사이의 의존성을 줄이기 위해 인터페이스에 맞춤
-	Menu pancakeHouseMenu;
-	Menu dinerMenu;
-	Menu cafeMenu;
+//	Menu pancakeHouseMenu;
+//	Menu dinerMenu;
+//	Menu cafeMenu;
 	
-	public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
-		this.pancakeHouseMenu = pancakeHouseMenu;
-		this.dinerMenu = dinerMenu;
-		this.cafeMenu = cafeMenu;
+	//메뉴를 List에 담아 처리함으로서 새로 생기는 메뉴가 생겨도 수정하지 않아도 됨
+	List<Menu> menus;
+	
+//	public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
+//		this.pancakeHouseMenu = pancakeHouseMenu;
+//		this.dinerMenu = dinerMenu;
+//		this.cafeMenu = cafeMenu;
+//	}
+	
+	public Waitress(List<Menu> menus) {
+		this.menus = menus;
 	}
 	
+//	public void printMenu() {
+//		java.util.Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
+//		java.util.Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
+//		java.util.Iterator<MenuItem> cafeIterator = cafeMenu.createIterator();
+//		
+//		System.out.println("메뉴\n----\n아침 메뉴");
+//		printMenu(pancakeIterator);
+//		System.out.println("\n점심 메뉴");
+//		printMenu(dinerIterator);
+//		System.out.println("\n저녁 메뉴");
+//		printMenu(cafeIterator);
+//	}
+	
+	//List의 Iterator를 가져와 반복으로 처리
 	public void printMenu() {
-		java.util.Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
-		java.util.Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
-		java.util.Iterator<MenuItem> cafeIterator = cafeMenu.createIterator();
+		Iterator<Menu> menuIterator = menus.iterator();
 		
-		System.out.println("메뉴\n----\n아침 메뉴");
-		printMenu(pancakeIterator);
-		System.out.println("\n점심 메뉴");
-		printMenu(dinerIterator);
-		System.out.println("\n저녁 메뉴");
-		printMenu(cafeIterator);
+		while(menuIterator.hasNext()) {
+			Menu menu = menuIterator.next();
+			printMenu(menu.createIterator());
+		}
 	}
 	
+	//각각 알맞게 구현한 Iterator를 받아와 작업
 	private void printMenu(java.util.Iterator<MenuItem> iterator) {
 		while(iterator.hasNext()) {
 			MenuItem menuItem = iterator.next();
