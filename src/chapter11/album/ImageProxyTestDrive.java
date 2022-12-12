0a320f5d0bba3e328c32b224b3529084b1609446
akm4545 <akm4545@naver.com>
2022-12-12 19:41:08 +0900

    chapter11

--- src/chapter11/album/ImageProxyTestDrive.java
+++ src/chapter11/album/ImageProxyTestDrive.java
@@ -10,6 +10,11 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
+//가상 프록시
+//생성하는데 많은 비용이 드는 객체를 대신한다.
+//진짜 객체가 필요한 상황이 오기 전까지 객체의 생성을 미루는 기능을 제공한다.
+//객체 생성 전이나 객체 생성 도중에 객체를 대신하기도 한다.
+//객체 생성이 끝나면 RealSubject에 직접 요청을 전달한다.
 public class ImageProxyTestDrive {
 	ImageComponent imageComponent;
 	JFrame frame = new JFrame("Album Cover Viewer");
@@ -39,14 +44,20 @@ public class ImageProxyTestDrive {
 			String name = (String)e.nextElement();
 			JMenuItem menuItem = new JMenuItem();
 			menu.add(menuItem);
+			
+			//이미지 프록시가
 			menuItem.addActionListener(event -> {
 				imageComponent.setIcon(new ImageProxy(getAlbumUrl(event.getActionCommand())));
 				frame.repaint();
 			});
 		}
 		
+		//초기 URL 지정
 		Icon icon = new ImageProxy(initialURL);
+		//프록시를 감싸서 생성
 		imageComponent = new ImageComponent(icon);
+		//프록시가 화면에 표시되도록 함
+		//이미지가 로딩되기 전에는 기본 메세지를 출력
 		frame.getContentPane().add(imageComponent);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(800, 600);
